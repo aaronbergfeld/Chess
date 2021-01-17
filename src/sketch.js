b8055cceadaf14ec415ff58f6e14003fdb09b989
Aaron <aaronbergfeld@gmail.com>
2021-01-17 02:21:11 -0500

    refactored more after looking up actual project structure

--- src/sketch.js
+++ src/sketch.js
@@ -1,11 +1,6 @@
 var viewHeight = window.innerHeight;
 var viewWidth = window.innerWidth;
 
-var chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-var nums = ['1', '2', '3', '4', '5', '6', '7', '8'];
-nums = nums.reverse();
-var offset = 50;
-
 var gameBoard = new Board();
 
 function setup() {
