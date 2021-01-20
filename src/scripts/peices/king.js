ef51e9694219e69613e4dc394a2aac01d4b32bcb
Aaron <aaronbergfeld@gmail.com>
2021-01-20 15:13:31 -0500

    added king movement

--- src/scripts/peices/king.js
+++ src/scripts/peices/king.js
@@ -5,4 +5,25 @@ class King extends Piece {
             this.pieceType = "king"
             this.source = `src/assets/pieces/${color}/${this.pieceType}.png`
         }
+
+        validMove(newBoardSquare) {
+            var newCoords = newBoardSquare.coordinates;
+            var oldX = this.coordinates[0];
+            var oldY = this.coordinates[1];
+            var newY = newCoords[1];
+            var newX = newCoords[0];
+            var kingMove = (newCoords.equals([oldX+1, oldY+1])) || 
+                           (newCoords.equals([oldX+1, oldY])) || 
+                           (newCoords.equals([oldX+1, oldY-1])) || 
+                           (newCoords.equals([oldX, oldY-1])) || 
+                           (newCoords.equals([oldX-1, oldY-1])) || 
+                           (newCoords.equals([oldX-1, oldY])) || 
+                           (newCoords.equals([oldX-1, oldY+1])) || 
+                           (newCoords.equals([oldX, oldY+1]));
+
+            if (kingMove){
+                return this.checkForPiece(newBoardSquare);
+            }
+            else return false
+        }
     }
